5a035bfcd96304ce7f2c15331bb594bc7490c25b
Antonio Ritacco <ritacco.ant@gmail.com>
2017-11-19 20:17:58 +0100

    To add: selective skip

---
+++ CSVparser/LoadMatrix.cs
@@ -0,0 +1,50 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSVparser
+{
+    class LoadMatrix
+    {
+        string path;
+        int skip;
+        public event EventHandler PartitionComplete;
+        AutoResetEvent done = new AutoResetEvent(false);
+        int running = 1;
+
+        public LoadMatrix(string path, int skip)
+        {
+            this.path = path;
+            this.skip = skip;
+        }
+
+        public Matrix<double> loadM()
+        {
+            var reader = new TXTparsercs();
+            var mReal1 = Matrix<double>.Build.DenseOfArray(reader.readCSV(skip, path));
+            if (0 == Interlocked.Decrement(ref running))
+                done.Set();
+            return mReal1;
+        }
+        public void OnGenerateFinish()
+        {
+            done.WaitOne();
+            OnEvent();
+
+        }
+
+        private void OnEvent()
+        {
+            if (PartitionComplete != null)
+            {
+                PartitionComplete(this, EventArgs.Empty);
+
+            }
+        }
+
+    }
+}
---
+++ CSVparser/TXTparsercs.cs
@@ -0,0 +1,126 @@
+﻿
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace CSVparser
+{
+    public class TXTparsercs
+    {
+        string[] fileFrom;
+        double[,] matrix;
+        int DIMR = 0;
+        int DIMC = 0;
+
+        public TXTparsercs(int i, int j)
+        {
+            DIMR = i;
+            DIMC = j;
+        }
+        public TXTparsercs() { }
+
+
+        public string[] readLabel(string path)
+        {
+            try
+            {
+                var csv = new StreamReader(path);
+                string fields = csv.ReadLine();
+                var values = fields.Split(' ');
+                DIMC = values.Length;
+                return values;
+
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+
+        }
+        public double[,] readCSV(int skip, string path)
+        {
+            var csv = new StreamReader(path);
+            var csvAux = new StreamReader(path);
+            DIMR -= skip;
+            string aux;
+            string[] count;
+            while (!csvAux.EndOfStream)
+            {
+                DIMR++;
+                aux = csvAux.ReadLine();
+                count = aux.Split(',');
+                DIMC = count.Count();
+            }
+            matrix = new double[DIMR, DIMC];
+
+            int k = 0;
+            for (int i = 0; i < skip; i++)
+            {
+                csv.ReadLine();
+            }
+            while (!csv.EndOfStream)
+            {
+                string fields = csv.ReadLine();
+                var values = fields.Split(',');
+                int j = 0;
+                double[] myDoubles = new double[DIMC];
+                foreach (string s in values)
+                {
+                    myDoubles[j] = Double.Parse(s, NumberStyles.Any, CultureInfo.InvariantCulture);
+                    j++;
+                }
+                for (int i = 0; i < DIMC; i++)
+                {
+                    matrix[k, i] = myDoubles[i];
+                }
+                k++;
+            }
+            csv.Close();
+            return matrix;
+        }
+
+
+
+
+
+
+
+
+        public void createFile(String src)
+        {
+            string[] lines = new string[DIMR];
+            // WriteAllLines creates a file, writes a collection of strings to the file,
+            // and then closes the file.  You do NOT need to call Flush() or Close().
+            Random r = new Random();
+            for (int i = 0; i < DIMR; i++)
+            {
+                string line = "";
+                for (int j = 0; j < DIMC; j++)
+                {
+                    if (j == DIMC - 1)
+                    {
+                        double d = r.NextDouble();
+                        line += (d.ToString());
+                    }
+                    else
+                    {
+                        double d = r.NextDouble();
+                        line += (d.ToString() + " ");
+                    }
+                }
+                lines[i] = line;
+                //Console.WriteLine(line);
+            }
+            try
+            {
+                System.IO.File.WriteAllLines(@"../../matrix.txt", lines);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
